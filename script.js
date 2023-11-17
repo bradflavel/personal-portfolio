a3210c52808b0f47f1c96e22a0a8dc7a7e2d873a
bradflavel <98391226+bradflavel@users.noreply.github.com>
2023-11-17 19:27:03 +1000

    Added cursor trail to website

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById(sectionId).style.display = 'block';
     }
 
-    // Add event listeners for hover
     document.querySelectorAll('.menu-item').forEach(function(menuItem) {
         menuItem.addEventListener('mouseover', function() {
             if (lastClickedSectionId !== menuItem.getAttribute('data-section')) {
@@ -29,4 +28,47 @@ document.addEventListener("DOMContentLoaded", function() {
             changeContent(lastClickedSectionId);
         });
     });
+
+    // Corrected canvas selection
+    const canvas = document.querySelector('.canvas');
+
+    document.addEventListener('mousemove', (e) => {
+        createParticle(e);
+    });
+
+    console.log
+
+    function createParticle(e) {
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        canvas.appendChild(particle);
+
+        // Particle size and position
+        const size = Math.random() * 10 + 5;
+        particle.style.width = `${size}px`;
+        particle.style.height = `${size}px`;
+        particle.style.left = `${e.pageX - size / 2}px`;
+        particle.style.top = `${e.pageY - size / 2}px`;
+
+        // Random neon color
+        const colors = ['var(--color-primary)', 'var(--color-secondary)', 'var(--color-tertiary)'];
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        particle.style.background = color;
+        particle.style.boxShadow = `0 0 10px ${color}, 0 0 20px ${color}, 0 0 30px ${color}`;
+        particle.style.border = `1px solid ${color}`;
+
+        // Animation
+        const animation = particle.animate([
+            { transform: 'translate(0, 0)', opacity: 1 },
+            { transform: 'translate(0, 20px)', opacity: 0 }
+        ], {
+            duration: 1000,
+            easing: 'ease-out'
+        });
+
+        animation.onfinish = () => {
+            particle.remove();
+        };
+    }
+    console.log
 });
